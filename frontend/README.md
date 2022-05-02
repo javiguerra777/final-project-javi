5184956e8d12e4cf264ab197391b379175941fec
Javier Guerra <javier.guerra1001@gmail.com>
2022-05-01 22:08:28 -0700

    front and backend with minor bugs

---
+++ frontend/README.md
@@ -0,0 +1,30 @@
+
+There are a few bugs in the code I am trying to workout, but the login works.
+For some reason I keep getting an error 401 code saying the logged in user is not authenticatedgit 
+# Posts
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.1.
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+
+## Code scaffolding
+
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
+
+## Running unit tests
+
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Running end-to-end tests
+
+Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
---
+++ frontend/src/app/components/create-post/create-post.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Output, ViewChild, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+
+import { Post } from 'src/app/models/Post';
+import { AuthService } from 'src/app/services/auth.service';
+import { PostService } from 'src/app/services/post.service';
+import { first } from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-create-post',
+  templateUrl: './create-post.component.html',
+  styleUrls: ['./create-post.component.scss']
+})
+export class CreatePostComponent implements OnInit {
+  @ViewChild("formDirective") formDirective!: NgForm;
+  @Output() create: EventEmitter<any> = new EventEmitter();
+  form!: FormGroup;
+  isOpen = false;
+  constructor(private authService: AuthService, private postService: PostService) { }
+
+  ngOnInit(): void {
+    this.form = this.createFormGroup();
+  }
+  
+  createFormGroup(): FormGroup{
+    return new FormGroup({
+      title: new FormControl("", [Validators.required, Validators.minLength(5)]),
+      body: new FormControl("", [Validators.required, Validators.minLength(10)])
+    })
+  };
+
+  onSubmit(formData: Pick<Post, "title" | "body">):void {
+    this.postService.createPost(formData, this.authService.userId)
+    .pipe(first()).subscribe(()=> this.create.emit(null));
+    this.form.reset();
+    this.formDirective.resetForm();
+  }
+}
