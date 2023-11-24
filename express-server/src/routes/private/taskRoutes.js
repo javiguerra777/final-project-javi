d549a7aa962f1e263f09717c177603585dbef550
Javier Guerra <javier.guerra1001@gmail.com>
2023-11-23 20:48:53 -0800

    feat(tasks routes): user able to patch task to update the name and or status, user able to delete task from database

--- express-server/src/routes/private/taskRoutes.js
+++ express-server/src/routes/private/taskRoutes.js
@@ -2,27 +2,52 @@ const express = require('express');
 const Task = require('../../models/Task.js');
 const router = express.Router();
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks/:projectId', async (req, res) => {
   try {
-    const tasks = await Task.findAll({ where: { userId: req.user.id, projectId: req.query.projectId }});
+    const tasks = await Task.findAll({ where: { userId: req.user.id, projectId: req.params.projectId }});
     res.status(200).json(tasks);
-  }catch(err){
+  } catch(err){
     console.log(err);
     res.status(500).send({ message: err.message });
   }
 });
 
-router.post('/tasks', async (req, res) => {
+router.post('/tasks/:projectId', async (req, res) => {
   try {
     const payload = {
       userId: req.user.id,
-      projectId: req.body.projectId,
+      projectId: req.params.projectId,
       name: req.body.name,
-      status: req.body.status,
+      status: req.body.status.toLowerCase().trim(),
     }
+    console.log(payload);
     const task = await Task.create(payload);
     res.status(201).json(task);
-  }catch(err){
+  } catch(err){
+    console.log(err);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+router.patch('/tasks/:id', async (req, res) => {
+  try {
+    const payload = {
+      name: req.body.name,
+      status: req.body.status.toLowerCase().trim(),
+    }
+    const task = await Task.update(payload, { where: { id: req.params.id, userId: req.user.id }});
+    res.status(200).json(task);
+  } catch(err){
+    console.log(err);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+router.delete('/tasks/:id', async (req, res) => {
+  try {
+    await Task.destroy({ where: { id: req.params.id, userId: req.user.id }});
+    res.status(200).json({ message: 'Task deleted' });
+  } catch(err){
     console.log(err);
     res.status(500).send({ message: err.message });
   }
