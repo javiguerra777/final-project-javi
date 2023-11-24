028e1251082d7a9e68dda546e290dd15ecfa6b0a
Javier Guerra <javier.guerra1001@gmail.com>
2023-11-23 20:26:06 -0800

    feat(tasks routes): added post route to create new task

--- express-server/src/routes/private/taskRoutes.js
+++ express-server/src/routes/private/taskRoutes.js
@@ -4,8 +4,24 @@ const router = express.Router();
 
 router.get('/tasks', async (req, res) => {
   try {
-    const tasks = await Task.findAll({ where: { userId: req.user.id, projectId: req.body.projectId }});
+    const tasks = await Task.findAll({ where: { userId: req.user.id, projectId: req.query.projectId }});
     res.status(200).json(tasks);
+  }catch(err){
+    console.log(err);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+router.post('/tasks', async (req, res) => {
+  try {
+    const payload = {
+      userId: req.user.id,
+      projectId: req.body.projectId,
+      name: req.body.name,
+      status: req.body.status,
+    }
+    const task = await Task.create(payload);
+    res.status(201).json(task);
   }catch(err){
     console.log(err);
     res.status(500).send({ message: err.message });
