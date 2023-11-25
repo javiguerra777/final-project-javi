ce5712f871276d3f3bb0a0e3eefca00a6891b81c
Javier Guerra <javier.guerra1001@gmail.com>
2023-11-24 21:01:53 -0800

    fix(get projects): decided to not cache projects at this moment until bug is fixed

--- frontend/src/app/services/projects.service.ts
+++ frontend/src/app/services/projects.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { getBaseApiUrl } from 'src/environments/environment.api';
-import { shareReplay, Observable, tap, BehaviorSubject, finalize, EMPTY } from 'rxjs';
 import { LocalStorageService } from './local-storage.service';
 
 type NewProject = {
@@ -11,37 +10,15 @@ type NewProject = {
   providedIn: 'root'
 })
 export class ProjectsService {
-  private loading = new BehaviorSubject<boolean>(false);
-  private projectsData: Observable<any> = EMPTY;
-
   constructor(
     private http: HttpClient,
     private localStorageService: LocalStorageService,
     ) { }
     getProjects() {
-      if (this.projectsData === EMPTY) {
-        this.loading.next(true);
-        this.projectsData = this.http.get(getBaseApiUrl() + '/api/projects',  this.getHeaders())
-        .pipe(
-          shareReplay(1), // Cache the response
-          finalize(() => this.loading.next(false))
-        );
-      }
-      return this.projectsData;
+      return this.http.get(getBaseApiUrl() + '/api/projects',  this.getHeaders());
     }
     createNewProject(data: NewProject) {
-      return this.http.post(getBaseApiUrl() + '/api/projects', data,  this.getHeaders())
-      .pipe(
-        tap(() => this.projectsData = this.fetchProjects()),
-        finalize(() => this.loading.next(false))
-      );
-    }
-    private fetchProjects() {
-      return this.http.get(getBaseApiUrl() + '/api/projects', this.getHeaders())
-      .pipe(
-        shareReplay(1), // Cache the response
-        finalize(() => this.loading.next(false))
-      );
+      return this.http.post(getBaseApiUrl() + '/api/projects', data,  this.getHeaders());
     }
     private getHeaders() {
       const localStorageState = this.localStorageService.getData();
